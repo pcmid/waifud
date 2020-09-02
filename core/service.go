db18eb22ed941d348f0ec4febe0465039f0b7c2b
pcmid <plzcmid@gmail.com>
2020-09-02 16:39:32 +0800

    refactor

--- core/service.go
+++ core/service.go
@@ -11,25 +11,20 @@ func Register(s Service) {
 }
 
 type R interface {
-	Reply(source Message, message Message)
 	Handle(message Message)
 	PostHandle(message Message)
 }
 
 type Receiver struct{}
 
-func (r *Receiver) Reply(source Message, message Message) {
-	source.Get("_response").(chan Message) <- message
-}
-
 func (r *Receiver) PostHandle(message Message) {
-	close(message.Get("_response").(chan Message))
+	message.Close()
 }
 func (r *Receiver) Handle(message Message) {}
 
 type S interface {
 	SetMessageChan(chan Message)
-	Send(message Message) (response Message)
+	Send(message Message) Message
 }
 
 type Sender struct {
@@ -43,10 +38,8 @@ func (s *Sender) SetMessageChan(c chan Message) {
 func (s *Sender) Send(message Message) Message {
 	channel := make(chan Message)
 	message.Set("_response", channel)
-
 	s.senderChan <- message
-
-	return <-channel
+	return message
 }
 
 type Service interface {
